b05246139d800a34048a91354ceab0a4280a1094
tb216500 <64595535+tb216500@users.noreply.github.com>
2023-04-27 19:58:53 +0200

    Update streamlit_app.py
    Adeded text line x3

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,6 @@ import streamlit
 streamlit.title('My parents new healthy dinner')
 
 streamlit.header('Breakfest Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Rang Egg')
