ce8c0c1fe7955240c9ba9313067a9cf759c00d24
tb216500 <64595535+tb216500@users.noreply.github.com>
2023-04-30 13:31:13 +0200

    Update streamlit_app.py
    Fix spelling error

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ streamlit.dataframe(fruits_to_show)
 # New section to display Fruityvice api resonse
 streamlit.header("Fruityvice Fruit Advice!")
 try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
       streamlit.error("Please select a fruit to get information.")
   else:
@@ -36,7 +36,7 @@ try:
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     streamlit.dataframe(fruityvice_normalized)
   
-execpt URLError as e:
+except URLError as e:
   streamlit.error()
      
   
