616b2cb7c4c91e529f5df59d00d30938f7fe32f9
tb216500 <64595535+tb216500@users.noreply.github.com>
2023-04-30 13:04:33 +0200

    Update streamlit_app.py
    Test fix added quotation mark to end of text

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,5 +44,5 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
 # allow the end user to add a fruit to the list
-add_my_fruit = streamlit.text_input('What fruit would you to add?)
-# streamlit.write('The user entered ', add_my_fruit)
+add_my_fruit = streamlit.text_input('What fruit would you to add?')
+streamlit.write('The user entered ', add_my_fruit)
